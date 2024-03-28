18ab7cacaa65c65829d4bec0aa474bcf347f1749
typoscript <minkyu.kim.contact@gmail.com>
2024-03-28 18:25:59 +0900

    Add printStartMenu(), printUserMenu(), printAccountMenu() Methods

--- console-bbs/src/main/SystemManager.java
+++ console-bbs/src/main/SystemManager.java
@@ -16,4 +16,23 @@ public class SystemManager {
 	public void run() {
 		
 	}
+	
+	private void printStartMenu() {
+		System.out.println("[1] 회원가입");
+		System.out.println("[2] 로그인");
+		System.out.println("[0] 종료");
+	}
+	
+	private void printUserMenu() {
+		System.out.println("[1] 로그아웃");
+		System.out.println("[2] 계정 관리");
+		System.out.println("[3] 게시글 보기");
+		System.out.println("[0] 종료");
+	}
+	
+	private void printAccountMenu() {
+		System.out.println("[1] 회원탈퇴");
+		System.out.println("[2] 정보 수정");
+		System.out.println("[3] 뒤로가기");
+	}
 }
