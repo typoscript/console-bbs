da2ad6a82ece4597da3029b4e9dadf2dfc9f1f20
typoscript <minkyu.kim.contact@gmail.com>
2024-03-28 17:57:13 +0900

    Add Getter & Setter Methods

--- console-bbs/src/main/User.java
+++ console-bbs/src/main/User.java
@@ -10,4 +10,20 @@ public class User {
 		this.id = id;
 		this.password = password;
 	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
