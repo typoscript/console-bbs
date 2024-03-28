6f4cb156f253e0a81fc7d950e83d6aa350881353
typoscript <minkyu.kim.contact@gmail.com>
2024-03-28 18:16:56 +0900

    Add Constructor Method

--- console-bbs/src/main/SystemManager.java
+++ console-bbs/src/main/SystemManager.java
@@ -1,9 +1,15 @@
 package main;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class SystemManager {
 	private int loggedInUserUID = -1;
 	private Board board;
 	private Set<User> users;
+	
+	public SystemManager() {
+		this.board = new Board();
+		this.users = new HashSet<User>();
+	}
 }
