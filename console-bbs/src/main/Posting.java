993d60a18c3888d0cc350cc8786b8636c862b76a
typoscript <minkyu.kim.contact@gmail.com>
2024-03-28 18:08:22 +0900

    Add Constructor Method

--- console-bbs/src/main/Posting.java
+++ console-bbs/src/main/Posting.java
@@ -3,4 +3,9 @@ package main;
 public class Posting {
 	private final int USER_UID;
 	private String content;
+	
+	public Posting(int userUID, String content) {
+		this.USER_UID = userUID;
+		this.content = content;
+	}
 }
