2225534b0f764f03b9c2f3944e1d74942919f956
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 16:12:49 +0900

    Modify getIndexOfPosting() Method

--- console-bbs/src/main/Board.java
+++ console-bbs/src/main/Board.java
@@ -57,9 +57,8 @@ public class Board {
 	
 	private int getIndexOfPosting(Posting posting) {
 		for (int i = 0; i < postings.size(); i++) {
-			if (postings.get(i).getId() == posting.getId()) {
+			if (postings.get(i).getId() == posting.getId())
 				return i;
-			}
 		}
 		
 		return -1;
