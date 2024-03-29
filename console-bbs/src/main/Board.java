c60e24ab2b3a0d3b4c4fd77e7219abc87f689dd7
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 14:07:53 +0900

    Add viewPostingAll() Method

--- console-bbs/src/main/Board.java
+++ console-bbs/src/main/Board.java
@@ -5,6 +5,24 @@ import java.util.ArrayList;
 public class Board {
 	private ArrayList<Posting> postings = new ArrayList<>();
 	
+	public void viewPostings() {
+		if (postings.isEmpty()) {
+			System.out.println("글이 없습니다");
+			return;
+		}
+		
+		for (int i = 0; i < postings.size(); i++) {
+			Posting posting = postings.get(i);
+			
+			String postingPreview = String.format(
+					"%d) 제목: %s | 글쓴이: %s | 업로드 날짜: %s\n",
+					i + 1, posting.getTitle(), posting.getUserId(), posting.getUploadDate()
+				);
+			
+			System.out.println(postingPreview);
+		}
+	}
+	
 	public boolean addPosting(Posting posting) {
 		postings.add(posting);
 		
@@ -18,6 +36,7 @@ public class Board {
 			return false;
 		
 		postings.remove(index);
+
 		return true;
 	}
 	
