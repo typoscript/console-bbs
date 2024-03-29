5d989c124466f437f883dc377ed0a47f3e42d7de
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 11:06:36 +0900

    Add static Keyword to Methods

--- console-bbs/src/main/Menu.java
+++ console-bbs/src/main/Menu.java
@@ -14,20 +14,20 @@ public class Menu {
 	public final int EDIT_USER = 2;
 	public final int GO_BACK = 3;
 
-	public void printStartMenu() {
+	public static void printStartMenu() {
 		System.out.println("[1] 회원가입");
 		System.out.println("[2] 로그인");
 		System.out.println("[0] 종료");
 	}
 	
-	public void printMainMenu() {
+	public static void printMainMenu() {
 		System.out.println("[1] 로그아웃");
 		System.out.println("[2] 계정 관리");
 		System.out.println("[3] 게시글 보기");
 		System.out.println("[0] 종료");
 	}
 	
-	public void printUserMenu() {
+	public static void printUserMenu() {
 		System.out.println("[1] 회원탈퇴");
 		System.out.println("[2] 정보 수정");
 		System.out.println("[3] 뒤로가기");
