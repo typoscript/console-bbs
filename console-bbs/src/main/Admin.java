4a31923bb89d23ba948fb35936431c2b1b81bb27
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 17:22:13 +0900

    Add clone() Method

--- console-bbs/src/main/Admin.java
+++ console-bbs/src/main/Admin.java
@@ -4,7 +4,7 @@ public class Admin extends User {
 	public Admin(String id, String password) {
 		super(id, password);
 	}
-	
+
 	@Override
 	public Admin clone() {
 		return new Admin(getId(), getPassword());
