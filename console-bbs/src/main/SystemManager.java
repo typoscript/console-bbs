5bbd1722db57b4a7e3d2aef4fb5c990301ba3c7e
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 11:05:39 +0900

    Remove print() Methods

--- console-bbs/src/main/SystemManager.java
+++ console-bbs/src/main/SystemManager.java
@@ -140,24 +140,5 @@ public class SystemManager {
 				return Integer.parseInt(input);
 			} catch (Exception e) { }
 		}
-	}
-	
-	private void printStartMenu() {
-		System.out.println("[1] 회원가입");
-		System.out.println("[2] 로그인");
-		System.out.println("[0] 종료");
-	}
-	
-	private void printMainMenu() {
-		System.out.println("[1] 로그아웃");
-		System.out.println("[2] 계정 관리");
-		System.out.println("[3] 게시글 보기");
-		System.out.println("[0] 종료");
-	}
-	
-	private void printUserMenu() {
-		System.out.println("[1] 회원탈퇴");
-		System.out.println("[2] 정보 수정");
-		System.out.println("[3] 뒤로가기");
-	}
+	}	
 }
