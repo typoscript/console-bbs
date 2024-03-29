ddf38c3bfbdc1127559d556614859b99f28f6779
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 10:42:23 +0900

    Add Member Variable sdf and getCurrentDate() Method

--- console-bbs/src/main/Date.java
+++ console-bbs/src/main/Date.java
@@ -1,4 +1,11 @@
 package main;
 
+import java.text.SimpleDateFormat;
+
 public class Date {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
+
+	public static String getCurrentDate() {
+		return sdf.format(System.currentTimeMillis());
+	}
 }
