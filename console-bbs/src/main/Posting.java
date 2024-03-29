80cec94be71be889b5231bb04cf2582673681842
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 10:36:40 +0900

    Add Member Variables: UPLOAD_DATE, title

--- console-bbs/src/main/Posting.java
+++ console-bbs/src/main/Posting.java
@@ -2,11 +2,14 @@ package main;
 
 public class Posting {
 	private final int USER_ID;
+	private final String UPLOAD_DATE;
+	private String title;
 	private String content;
 	
 	public Posting(int userId, String content) {
 		this.USER_ID = userId;
 		this.content = content;
+		this.UPLOAD_DATE = Date.getCurrentDate();
 	}
 	
 	public int getUserID() {
