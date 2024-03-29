e46f6b0f3d871d9f1bf1d123ac151dbdb74d6371
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 17:36:16 +0900

    Add canDeletePosting() Method

--- console-bbs/src/main/SystemManager.java
+++ console-bbs/src/main/SystemManager.java
@@ -263,4 +263,10 @@ public class SystemManager {
 		
 		System.out.println("글 수정 성공");
 	}
+	
+	private boolean canDeletePosting(Posting posting) {
+		User user = userManager.getUser(loggedInUserId);
+
+		return !userManager.isAdminUser(user) && !posting.getUserId().equals(loggedInUserId);
+	}
 }
