51cebae0e577e2c543c5a0a5e2303f8a72a14a2f
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 12:25:01 +0900

    Add getString(), getNumber() Methods

--- console-bbs/src/main/Input.java
+++ console-bbs/src/main/Input.java
@@ -1,5 +1,24 @@
 package main;
 
-public class Input {
+import java.util.Scanner;
 
+public class Input {
+	private static Scanner sc = new Scanner(System.in);
+	
+	private Input() { }
+
+	public static String getString(String msg) {
+		System.out.print(msg + ": ");
+		return sc.next();
+	}
+
+	public static int getNumber(String msg) {
+		while (true) {
+			String input = getString(msg);
+
+			try {
+				return Integer.parseInt(input);
+			} catch (Exception e) { }
+		}
+	}	
 }
