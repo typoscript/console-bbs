30d2a4c3a2ddc6da2e39f0aa883014a1a1992cbf
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 12:04:05 +0900

    Add getIndexOfPosting()

--- console-bbs/src/main/Board.java
+++ console-bbs/src/main/Board.java
@@ -12,13 +12,23 @@ public class Board {
 	}
 	
 	public boolean deletePosting(Posting posting) {
-		for (int i = 0; i < postings.size(); i++) {
-			if (postings.get(i).getId() == posting.getId()) {
-				postings.remove(i);
-				return true;
-			}
+		int index = getIndexOfPosting(posting);
+		
+		if (index >= 0) {
+			postings.remove(index);
+			return true;
 		}
 		
 		return false;
+	}
+	
+	private int getIndexOfPosting(Posting posting) {
+		for (int i = 0; i < postings.size(); i++) {
+			if (postings.get(i).getId() == posting.getId()) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 }
