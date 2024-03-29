85e3a9baaf64050283e381e441a4234673309cd4
typoscript <minkyu.kim.contact@gmail.com>
2024-03-29 12:47:29 +0900

    Modify editUser() Method

--- console-bbs/src/main/UserManager.java
+++ console-bbs/src/main/UserManager.java
@@ -40,19 +40,21 @@ public class UserManager {
 		return true;
 	}
 	
-	public void editUser(String userId) {
+	public boolean editUser(String userId) {
 		String password = Input.getString("새 비밀번호");
 		String reEnteredPassword = Input.getString("새 비밀번호 재입력");
 		
 		if (!password.equals(reEnteredPassword)) {
 			System.out.println("비밀번호 불일치");
-			return;
+			return false;
 		}
 		
 		User user = users.get(userId);
 		user.setPassword(password);
 		
 		users.replace(userId, user);
+		
+		return true;
 	}	
 	
 	public boolean hasId(String id) {
