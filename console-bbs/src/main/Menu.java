eb3c8f2964704e4f680251fcbf296232292ca503
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 11:41:07 +0900

    Change Access Modifier: public -> protected

--- console-bbs/src/main/Menu.java
+++ console-bbs/src/main/Menu.java
@@ -1,43 +1,43 @@
 package main;
 
 public class Menu {
-	public static final int EXIT = 0;
+	protected final int EXIT = 0;
 
-	public static final int ADD_USER = 1;
-	public static final int LOG_IN = 2;
+	protected final int ADD_USER = 1;
+	protected final int LOG_IN = 2;
 
-	public static final int LOG_OUT = 1;
-	public static final int MANAGE_USER = 2;
-	public static final int VIEW_POSTING_ALL = 3;
+	protected final int LOG_OUT = 1;
+	protected final int MANAGE_USER = 2;
+	protected final int VIEW_POSTING_ALL = 3;
 
-	public static final int DELETE_USER = 1;
-	public static final int EDIT_USER = 2;
-	public static final int GO_BACK = 3;
+	protected final int DELETE_USER = 1;
+	protected final int EDIT_USER = 2;
+	protected final int GO_BACK = 3;
 	
-	public static final int VIEW_POSTING = 1;
-	public static final int ADD_POSTING = 2;
+	protected final int VIEW_POSTING = 1;
+	protected final int ADD_POSTING = 2;
 
-	public static final int EDIT_POSTING = 1;
-	public static final int DELETE_POSTING = 2;
+	protected final int EDIT_POSTING = 1;
+	protected final int DELETE_POSTING = 2;
 	
-	public static final int ADMIN_MANAGE_USER = 4;
+	protected final int ADMIN_MANAGE_USER = 4;
 
-	public static final int ADMIN_VIEW_USER_ALL = 1;
+	protected final int ADMIN_VIEW_USER_ALL = 1;
 
-	public static void printStartMenu() {
+	protected void printStartMenu() {
 		System.out.println("[1] 회원가입");
 		System.out.println("[2] 로그인");
 		System.out.println("[0] 종료");
 	}
 	
-	public static void printUserMainMenu() {
+	protected void printUserMainMenu() {
 		System.out.println("[1] 로그아웃");
 		System.out.println("[2] 계정 관리");
 		System.out.println("[3] 게시글 보기");
 		System.out.println("[0] 종료");
 	}
 
-	public static void printAdminMainMenu() {
+	protected void printAdminMainMenu() {
 		System.out.println("[1] 로그아웃");
 		System.out.println("[2] 계정 관리");
 		System.out.println("[3] 게시글 보기");
@@ -45,25 +45,25 @@ public class Menu {
 		System.out.println("[0] 종료");
 	}
 	
-	public static void printUserMenu() {
+	protected void printUserMenu() {
 		System.out.println("[1] 회원탈퇴");
 		System.out.println("[2] 정보 수정");
 		System.out.println("[3] 뒤로가기");
 	}
 
-	public static void printPostingMenu() {
+	protected void printPostingMenu() {
 		System.out.println("[1] 글 보기");
 		System.out.println("[3] 글쓰기");
 		System.out.println("[3] 뒤로가기");
 	}
 
-	public static void printPostingEditMenu() {
+	protected void printPostingEditMenu() {
 		System.out.println("[1] 글 수정");
 		System.out.println("[2] 글 삭제");
 		System.out.println("[3] 뒤로가기");
 	}
 	
-	public static void printUserManagementMenu() {
+	protected void printUserManagementMenu() {
 		System.out.println("[1] 유저 리스트 보기");
 		System.out.println("[3] 뒤로가기");
 	}
