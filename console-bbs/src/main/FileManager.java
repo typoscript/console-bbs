b6d7e571f7ab81695eeef47aa4cbb244a1b42315
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 10:26:56 +0900

    Add Member Variables

--- console-bbs/src/main/FileManager.java
+++ console-bbs/src/main/FileManager.java
@@ -8,6 +8,8 @@ public class FileManager {
 	private static FileReader fr;
 	private static FileWriter fw;
 	private static BufferedReader br;
+	private static final String USER_FILE = "user.txt";
+	private static final String BOARD_FILE = "board.txt";
 	
 	private FileManager() { }
 }
