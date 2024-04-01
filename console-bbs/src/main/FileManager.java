86619ee286f66834b8c6257f3edba946e6f06df4
typoscript <minkyu.kim.contact@gmail.com>
2024-04-01 10:31:55 +0900

    Rename Member Variables

--- console-bbs/src/main/FileManager.java
+++ console-bbs/src/main/FileManager.java
@@ -8,12 +8,12 @@ public class FileManager {
 	private static FileReader fr;
 	private static FileWriter fw;
 	private static BufferedReader br;
-	private static final String USER_FILE = "user.txt";
-	private static final String BOARD_FILE = "board.txt";
+	private static final String USER_FILE_NAME = "user.txt";
+	private static final String BOARD_FILE_NAME = "board.txt";
 	
 	private FileManager() { }
 
 	public static void saveUserDataToFile(String data) {
-		saveDataToFile(USER_FILE, data);
+		saveDataToFile(USER_FILE_NAME, data);
 	}
 }
